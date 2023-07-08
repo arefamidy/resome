0a8193b84372f5823659cb9503d6b523792dcd10
arefamidy <arefamidy.1382@gmail.com>
2023-07-08 03:59:14 -0400

    first commit

---
+++ tem.js
@@ -0,0 +1,10 @@
+function change(){
+    document.body.classList.toggle('dark')
+    if(body.classList=='dark'){
+        darkmode.innerhtml='dark'
+        
+
+    }else(
+        darkmode.innerhtml='ligth'
+    )
+}
